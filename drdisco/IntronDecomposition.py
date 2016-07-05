f5bcdfb2b66ced2ab0d9e41e1478a5f8b1241676
yhoogstrate <y.hoogstrate@gmail.com>
2016-07-05 16:00:01 +0200

    Progress..

--- drdisco/IntronDecomposition.py
+++ drdisco/IntronDecomposition.py
@@ -7,6 +7,34 @@
 import logging
 import pysam
 
+
+class Element:
+    def __init__(self,_chr,_pos):
+        self._chr = _chr
+        self._pos = _pos
+        arcs_fwd = []
+        arcs_rev = []
+
+
+class Chain:
+    def __init__(self):
+        idx = {}
+    
+    def insert(self,pos1,pos2,_type):
+        # Type:
+        # 1. 'N' <- N alignment flag in SAM, meaning INTRON
+        # 2. 'discordant_read'
+        # 3. 'split_read'
+        #
+        # ... 'S' and 'H' for soft and hard clipping?
+        pass
+    
+    def prune(self):
+        pass
+        # do some clever tricks to merge arcs together and reduce data points
+
+
+
 class IntronDecomposition:
     def __init__(self,break_point):
         self.logger = logging.getLogger(self.__class__.__name__)
@@ -26,12 +54,40 @@ class IntronDecomposition:
     def annotate_genes(self,gene_set):
         pass
     
+    def is_exonic(self,read,gene):
+        pass
+    
+    def get_insert_size(self,pos1,pos2):
+        if pos1[0] == pos2[0]:
+            return pos2[1] - pos1[1]
+        else:
+            return 99999999
+    
+    def parse_SA(self,SA_tag):
+        sa_tags = SA_tag.split(":")
+        for i in range(len(sa_tags)):
+            print sa_tags[i]
+            sa_tags[i] = sa_tags[i].split(",")
+            sa_tags[i][1] = int(sa_tags[i][1])
+            #sa_tags[i][2] = int(sa_tags[i][2])
+        
+        return sa_tags
+    
     def decompose(self,alignment_file):
         pysam_fh = self.test_disco_alignment(alignment_file)
         
         lpos = self.break_point.get_left_position(True)
         rpos = self.break_point.get_right_position(True)
         
+        chain_left = []
+        chain_right = []
+        
+        c = Chain()
+        
         for r in pysam_fh.fetch(lpos[0],lpos[1]):
-            print r.query_name, r.cigar, r.get_tag('RG')
+            print r.query_name
+            print " -" , r.reference_start, "-" if r.is_reverse else "+"
+            print " -" , r.cigarstring
+            print " -" , r.get_tag('RG')
+            print " -" , self.parse_SA(r.get_tag('SA'))
             #print r.get_aligned_pairs()
