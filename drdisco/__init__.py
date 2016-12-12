0057d79fa31eee200132971cb39c578de87ae34f
yhoogstrate <y.hoogstrate@gmail.com>
2016-12-12 17:05:43 +0100

    another improvement with respect to filtering

--- drdisco/__init__.py
+++ drdisco/__init__.py
@@ -40,6 +40,6 @@ MAX_GENOME_DISTANCE = 999999999
 
 
 # filter settings
-MIN_SUBNET_ENTROPY = 0.55
+MIN_SUBNET_ENTROPY = 0.585
 MIN_DISCO_PER_SUBNET_PER_NODE = 1  # minimum nodes is 2 per subnet, hence mininal 2 discordant reads are necessairy
 MIN_SUPPORTING_READS_PER_SUBNET_PER_NODE = 4  # minimum supporting reads is 8 per subnet
