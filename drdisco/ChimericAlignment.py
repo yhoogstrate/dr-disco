7c621de6ba0ffe4b0432573b5880d8d9f79ecfbb
yhoogstrate <y.hoogstrate@gmail.com>
2017-02-16 18:01:50 +0100

    GPL clarifications

--- drdisco/ChimericAlignment.py
+++ drdisco/ChimericAlignment.py
@@ -10,6 +10,31 @@ import pysam
 from fuma.Fusion import STRAND_FORWARD
 from .CigarAlignment import CigarAlignment
 
+"""[License: GNU General Public License v3 (GPLv3)]
+
+    Dr. Disco: fusion gene detection in random hexamer RNA-seq data
+    Copyright (C) 2017  Youri Hoogstrate
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+    You can contact me via the github repository at the following url:
+    <https://github.com/yhoogstrate/dr-disco>
+
+    You can e-mail me via 'yhoogstrate' at the following webmail domain:
+    gmail dot com
+"""
 
 """
  Sep 01: fix bam files:
