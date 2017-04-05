f22e677ede8d91c9669793893078ff1a401c8828
yhoogstrate <y.hoogstrate@gmail.com>
2017-04-05 13:25:52 +0200

    tm test 22

--- tests/test_intronic_break_detection.py
+++ tests/test_intronic_break_detection.py
@@ -438,59 +438,43 @@ class TestIntronicBreakDetection(unittest.TestCase):
         ic = IntronDecomposition(input_file_a)
         self.assertRaises(Exception, ic.decompose, 0)  # ic.decompose(0) triggers exception
 
-    #def test_21_tests_extracting_subnetworks_in_ideal_optimization_usecase(self):
-        #input_file_a = TEST_DIR + "test_21.bam"
+    def test_21_tests_extracting_subnetworks_in_ideal_optimization_usecase(self):
+        test_id = '21'
         
-        ## Dev stuff // insert
-        #sam = TEST_DIR + "test_21.sam"
-        #fixed_bam = T_TEST_DIR + "test_21.fixed.bam"
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
         
-        #sam_to_fixed_bam(sam, fixed_bam)
+        sam_to_fixed_bam(input_file_a, fixed_bam)
+
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
+
+        with open(output_file, "w") as fh:
+            ic.export(fh)
+
+        # Test data not checked, should just not throw an exception
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+
+    def test_22_interchromosomal_tp_sample_130(self):
+        test_id = '22'
         
-        #d = bam_diff(fixed_bam, input_file_a)
-        #print subprocess.Popen(['diff', d[1], d[2]], stdout=subprocess.PIPE).stdout.read()
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
         
-        #input_file_a = fixed_bam
-        ###
-        
-        #test_file = TEST_DIR + "test_21.out.dbed"
-        #output_file = T_TEST_DIR + "test_21.out.dbed"
+        sam_to_fixed_bam(input_file_a, fixed_bam)
 
-        #ic = IntronDecomposition(input_file_a)
-        #ic.decompose(0)
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
 
-        #with open(output_file, "w") as fh:
-            #ic.export(fh)
+        with open(output_file, "w") as fh:
+            ic.export(fh)
 
-        ## Test data not checked, should just not throw an exception
-        #self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
-
-    #def test_22_interchromosomal_tp_sample_130(self):
-        #input_file_a = TEST_DIR + "test_22.bam"
-
-        ## Dev stuff // insert
-        #sam = TEST_DIR + "test_22.sam"
-        #fixed_bam = T_TEST_DIR + "test_22.fixed.bam"
-        
-        #sam_to_fixed_bam(sam, fixed_bam)
-        
-        #d = bam_diff(fixed_bam, input_file_a)
-        #print subprocess.Popen(['diff', d[1], d[2]], stdout=subprocess.PIPE).stdout.read()
-        
-        #input_file_a = fixed_bam
-        ##
-
-        #test_file = TEST_DIR + "test_22.out.dbed"
-        #output_file = T_TEST_DIR + "test_22.out.dbed"
-
-        #ic = IntronDecomposition(input_file_a)
-        #ic.decompose(0)
-
-        #with open(output_file, "w") as fh:
-            #ic.export(fh)
-
-        ## Test data not checked, should just not throw an exception
-        #self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+        # Test data not checked, should just not throw an exception
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
 
     #def test_23_circRNA_s14(self):
         #input_file_a = TEST_DIR + "test_23.bam"
