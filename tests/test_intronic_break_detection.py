1bc1868c292c822a82dc09300caf65ead72778d0
yhoogstrate <y.hoogstrate@gmail.com>
2017-04-05 13:16:00 +0200

    test13

--- tests/test_intronic_break_detection.py
+++ tests/test_intronic_break_detection.py
@@ -267,83 +267,59 @@ class TestIntronicBreakDetection(unittest.TestCase):
 
         self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
 
-    #def test_11(self):
-        #input_file_a = TEST_DIR + "test_11.bam"
-
-        ## Dev stuff // insert
-        #sam = TEST_DIR + "test_11.sam"
-        #fixed_bam = T_TEST_DIR + "test_11.fixed.bam"
+    def test_11(self):
+        test_id = '11'
         
-        #sam_to_fixed_bam(sam, fixed_bam)
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
         
-        #d = bam_diff(fixed_bam, input_file_a)
-        #print subprocess.Popen(['diff', d[1], d[2]], stdout=subprocess.PIPE).stdout.read()
+        sam_to_fixed_bam(input_file_a, fixed_bam)
+
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
+
+        with open(output_file, "w") as fh:
+            ic.export(fh)
+
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+
+    def test_12_extract_subnetworks(self):
+        test_id = '12'
         
-        #input_file_a = fixed_bam
-        ##
-
-        #test_file = TEST_DIR + "test_11.out.dbed"
-        #output_file = T_TEST_DIR + "test_11.out.dbed"
-
-        #ic = IntronDecomposition(input_file_a)
-        #ic.decompose(0)
-
-        #with open(output_file, "w") as fh:
-            #ic.export(fh)
-
-        #self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
-
-    #def test_12_extract_subnetworks(self):
-        #input_file_a = TEST_DIR + "test_12.bam"
-
-        ## Dev stuff // insert
-        #sam = TEST_DIR + "test_12.sam"
-        #fixed_bam = T_TEST_DIR + "test_12.fixed.bam"
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
         
-        #sam_to_fixed_bam(sam, fixed_bam)
+        sam_to_fixed_bam(input_file_a, fixed_bam)
+
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
+
+        with open(output_file, "w") as fh:
+            ic.export(fh)
+
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+
+    def test_13_merge_overlapping_subnetworks(self):
+        test_id = '13'
         
-        #d = bam_diff(fixed_bam, input_file_a)
-        #print subprocess.Popen(['diff', d[1], d[2]], stdout=subprocess.PIPE).stdout.read()
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
         
-        #input_file_a = fixed_bam
-        ##
+        sam_to_fixed_bam(input_file_a, fixed_bam)
 
-        #test_file = TEST_DIR + "test_12.out.dbed"
-        #output_file = T_TEST_DIR + "test_12.out.dbed"
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
 
-        #ic = IntronDecomposition(input_file_a)
-        #ic.decompose(0)
+        with open(output_file, "w") as fh:
+            ic.export(fh)
 
-        #with open(output_file, "w") as fh:
-            #ic.export(fh)
-
-        #self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
-
-    #def test_13_merge_overlapping_subnetworks(self):
-        #input_file_a = TEST_DIR + "test_13.bam"
-
-        ## Dev stuff // insert
-        #sam = TEST_DIR + "test_13.sam"
-        #fixed_bam = T_TEST_DIR + "test_13.fixed.bam"
-        
-        #sam_to_fixed_bam(sam, fixed_bam)
-        
-        #d = bam_diff(fixed_bam, input_file_a)
-        #print subprocess.Popen(['diff', d[1], d[2]], stdout=subprocess.PIPE).stdout.read()
-        
-        #input_file_a = fixed_bam
-        ##
-
-        #test_file = TEST_DIR + "test_13.out.dbed"
-        #output_file = T_TEST_DIR + "test_13.out.dbed"
-
-        #ic = IntronDecomposition(input_file_a)
-        #ic.decompose(0)
-
-        #with open(output_file, "w") as fh:
-            #ic.export(fh)
-
-        #self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
 
     #def test_14_test_inserting_spanning_paired_12_s(self):
         #input_file_a = TEST_DIR + "test_14.bam"
