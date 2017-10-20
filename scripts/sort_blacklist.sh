ac986fcbf8fb74bf48cac141bd70c5eb9f255ee6
yhoogstrate <y.hoogstrate@gmail.com>
2017-10-20 16:35:18 +0200

    helper

--- scripts/sort_blacklist.sh
+++ scripts/sort_blacklist.sh
@@ -3,3 +3,6 @@
 sort -k 1.4,1 -s share/blacklist-regions.hg38.bed > tmp/a.bed
 mv tmp/a.bed share/blacklist-regions.hg38.bed
 
+sort -k 1.4,1 -s share/blacklist-junctions.hg38.txt > tmp/b.txt
+mv tmp/b.txt share/blacklist-junctions.hg38.txt
+
