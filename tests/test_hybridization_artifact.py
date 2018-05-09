b52c21b953cd65883a8ffdd3def692f1acc90abd
yhoogstrate <y.hoogstrate@gmail.com>
2018-05-09 18:52:10 +0200

    test case for artfact exclusion

---
+++ tests/test_hybridization_artifact.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# *- coding: utf-8 -*-
+# vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4 textwidth=79:
+
+"""
+Dr. Disco - testing `dr-disco detect ...`. This includes running `dr-disco fix ...` first to ensure the whole pipeline is in working.
+
+[License: GNU General Public License v3 (GPLv3)]
+
+ This file is part of Dr. Disco.
+
+ FuMa is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Dr. Disco is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from drdisco.IntronDecomposition import IntronDecomposition
+
+import unittest
+import subprocess
+import filecmp
+import os
+from utils import main, sam_to_fixed_bam
+
+
+subprocess.call(["bash", "tests/rm_bai_files.sh"])
+
+TEST_DIR = "tests/hybridization-artifact/"
+T_TEST_DIR = "tmp/" + TEST_DIR
+
+
+# Nosetests doesn't use main()
+if not os.path.exists(T_TEST_DIR):
+    os.makedirs(T_TEST_DIR)
+
+
+class TestIntronicBreakDetection(unittest.TestCase):
+    def test_01(self):
+        test_id = 'artifact_reads_01'
+
+        input_file_a = TEST_DIR + "test_" + test_id + ".sam"
+        fixed_bam = T_TEST_DIR + "test_" + test_id + ".fixed.bam"
+        test_file = TEST_DIR + "test_" + test_id + ".out.dbed"
+        output_file = T_TEST_DIR + "test_" + test_id + ".out.dbed"
+
+        sam_to_fixed_bam(input_file_a, fixed_bam, T_TEST_DIR)
+
+        ic = IntronDecomposition(fixed_bam)
+        ic.decompose(0)
+
+        fh = open(output_file, "w")
+        ic.export(fh)
+        fh.close()
+
+        self.assertTrue(filecmp.cmp(test_file, output_file), msg="diff '" + test_file + "' '" + output_file + "':\n" + subprocess.Popen(['diff', test_file, output_file], stdout=subprocess.PIPE).stdout.read())
+
+
+if __name__ == '__main__':
+    main()
